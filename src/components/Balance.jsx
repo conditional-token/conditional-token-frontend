1f3163190e4c858e36edb112bf7a1456d699481b
caiosbl <caio.lira@ccc.ufcg.edu.br>
2022-08-21 01:13:47 -0300

    Update Balance style

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -1,36 +1,56 @@
+import { useAuth } from "../contexts/auth";
 import { useMetamask } from "../contexts/metamask";
 import { colors } from "../utils/constants";
 
+import { MonetizationOn } from "@mui/icons-material";
+
 function Balance() {
   const { balance } = useMetamask();
+  const {user} = useAuth();
   return (
     <div style={styles.container}>
+      <span style={styles.user}>Hello, {user.name}!</span>
       <span style={styles.title}>Current Balance</span>
-      <span style={styles.balance}>{balance?.toFixed(2)} ETH</span>
+      <span style={styles.balance}>
+        <MonetizationOn />
+        {balance?.toFixed(2)}
+         <span style={{ fontSize: 12, marginLeft: 5}}>ETH</span>
+        </span>
     </div>
   );
 }
 
 const styles = {
   balance: {
-    fontFamily: "'Roboto', sans-serif",
-    fontSize: 50,
-    marginTop: 20
+    display: "flex",
+    alignItems: "center",
+    fontSize: 20,
+    marginRight: 20,
+    backgroundColor: colors.primaryDark,
+    color: colors.primaryLight,
+    padding: 5,
+    borderRadius: 20,
   },
   container: {
     display: "flex",
-    flexDirection: "column",
+    flexWrap: "wrap",
     alignItems: "center",
-    justifyContent: "center",
-    width: 400,
-    height: 200,
+    justifyContent: "flex-end",
+    width: "100%",
+    height: 50,
     backgroundColor: colors.primaryLight,
-    borderRadius: 10,
   },
   title: {
-    fontFamily: "'Roboto', sans-serif",
-    fontSize: 30,
+    fontSize: 12,
+    fontWeight: "700",
+    marginRight: 10,
+    fontWeight: "400",
   },
+  user: {
+    marginRight: "auto",
+    marginLeft: 10,
+    fontWeight: "700",
+  }
 };
 
 export default Balance;
